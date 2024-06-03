c844b9dcdb4a03501d6a5bbeea8a944c2b7f3cfa
johwangyu <165298876+hwangyujo@users.noreply.github.com>
2024-06-03 19:59:42 +0900

    Update Find_Number.py

--- Find_Number.py
+++ Find_Number.py
@@ -10,10 +10,4 @@ while True:
         except ValueError:
             print("유효한 숫자를 입력하세요.")
 
-    if user_input < random_number:
-        print("숫자가 너무 작습니다.")
-    elif user_input > random_number:
-        print("숫자가 너무 큽니다.")
-    else:
-        print("정답입니다!")
-        break
+
